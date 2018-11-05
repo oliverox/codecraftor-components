15e943237e8f2fb8fcd6467b581f4b9ddc4705b0
Oliver Oxenham <oliver@cardcity.co>
2018-11-05 23:16:47 +0400

    added handleAction to MainFrame

--- MainFrame/MainFrame.js
+++ MainFrame/MainFrame.js
@@ -1,8 +1,46 @@
 import React from 'react';
-import { Button, Radio, RadioGroup } from "@blueprintjs/core";
+import firebase from 'firebase/app';
+import shortid from 'shortid';
+import { Button, Radio, RadioGroup } from '@blueprintjs/core';
 import './MainFrame.scss';
 
 class MainFrame extends React.Component {
+  constructor() {
+    super();
+    this.handleAction = this.handleAction.bind(this);
+  }
+
+  componentWillMount() {
+    const { match } = this.props;
+    const db = firebase.firestore();
+    db.settings({ timestampsInSnapshots: true });
+    this.docRef = db
+      .collection(process.env.REACT_APP_CRAFTS_COLLECTION)
+      .doc(match.params.craftId);
+  }
+
+  handleAction(cmd, params) {
+    console.log('MainFrame - handleAction', cmd, params);
+    switch (cmd) {
+      case 'ADD':
+        this.docRef
+          .update({
+            actions: firebase.firestore.FieldValue.arrayUnion({
+              id: shortid.generate(),
+              action: 'ADD',
+              ...params
+            })
+          })
+          .catch(err => {
+            console.log('Error updating document.', err);
+          });
+        break;
+
+      default:
+        break;
+    }
+  }
+
   render() {
     const { match } = this.props;
     console.log('craft id =', match.params.craftId);
@@ -32,7 +70,7 @@ class MainFrame extends React.Component {
             <Button
               intent="primary"
               onClick={() =>
-                this.handleAction('add', {
+                this.handleAction('ADD', {
                   type: 'h1',
                   props: {},
                   children: 'This is an h1 header'
@@ -51,10 +89,25 @@ class MainFrame extends React.Component {
               Update
             </Button>
           </div>
+          <hr />
+          <h4>Components</h4>
+          <Button
+            intent="none"
+            onClick={() =>
+              this.handleAction('ADD', {
+                component: 'Button',
+                target: 'body'
+              })
+            }
+          >
+            Add Button
+          </Button>
         </div>
 
         <iframe
-          src={`${process.env.REACT_APP_CRAFT_FRAME_URL}/${match.params.craftId}`}
+          src={`${process.env.REACT_APP_CRAFT_FRAME_URL}/${
+            match.params.craftId
+          }`}
           width={500}
           height={600}
           title="Craft Frame"
