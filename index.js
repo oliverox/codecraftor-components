1ad822eabcf53cfea4e58c81bd23348df0eb8cae
Oliver Oxenham <oliver@cardcity.co>
2018-11-05 15:01:57 +0400

    added index

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 import Craft from './Craft/Craft';
+import Index from './Index/Index';
+import MainFrame from './MainFrame/MainFrame';
+import SpinnerOverlay from './SpinnerOverlay/SpinnerOverlay';
 
-export { Craft };
+export { Craft, Index, MainFrame, SpinnerOverlay };
