4812ebeb23c02c0dca0228c85a049e5fd1a82dd8
Oliver Oxenham <oliver@cardcity.co>
2018-11-06 15:08:36 +0400

    removed MainFrame from this repo

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import Craft from './Craft/Craft';
 import Index from './Index/Index';
-import MainFrame from './MainFrame/MainFrame';
 import SpinnerOverlay from './SpinnerOverlay/SpinnerOverlay';
 
-export { Craft, Index, MainFrame, SpinnerOverlay };
+export { Craft, Index, SpinnerOverlay };
