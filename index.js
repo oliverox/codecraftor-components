21931b91b2c858b4732233da5e5555037d9f4e45
Oliver Oxenham <oliver@cardcity.co>
2018-11-10 17:39:34 +0400

    removed Craft from repo

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import Craft from './Craft/Craft';
 import SpinnerOverlay from './SpinnerOverlay/SpinnerOverlay';
 
 // React -> Blueprint Components
 import Heading from './react/blueprint/Heading/Heading';
 
-export { Craft, SpinnerOverlay, Heading };
+export { SpinnerOverlay, Heading };
