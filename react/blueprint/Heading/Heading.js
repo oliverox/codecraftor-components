bc68b9555d0a78a3a4e112c008d70d85f6dd4eab
Oliver Oxenham <oliver@cardcity.co>
2018-11-15 09:38:20 +0400

    added H6 in Heading

--- react/blueprint/Heading/Heading.js
+++ react/blueprint/Heading/Heading.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { H1, H2, H3, H4, H5 } from '@blueprintjs/core';
+import { H1, H2, H3, H4, H5, H6 } from '@blueprintjs/core';
 
 class Heading extends React.Component {
   render() {
