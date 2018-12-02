03f6633cd69e1fd81aa73455295508212747956e
Oliver Oxenham <oliver@cardcity.co>
2018-12-02 15:12:42 +0400

    added a defaultStyle to Container

--- Container/Container.js
+++ Container/Container.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const Container = ({ children, className = '', style = {} }) => {
+  const defaultStyle = {
+    width: '100%',
+    height: 200,
+    backgroundColor: '#fff'
+  };
   return (
-    <div style={style} className={className}>
+    <div style={{ ...defaultStyle, ...style }} className={className}>
       {children}
     </div>
   );
