e28c9f98baf0ca35a4693d1fc743b5a82fe255a4
Oliver Oxenham <oliver@cardcity.co>
2018-12-03 11:32:21 +0400

    - dev styles in Container

--- Container/Container.js
+++ Container/Container.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
+if (process.env.NODE_ENV === 'development') {
+  import('./styles.css');
+}
+
 const Container = ({ children, className = '', style = {} }) => {
+  const devClassName = 'containerDev';
+  const isDev = process.env.NODE_ENV === 'development';
   const defaultStyle = {
     width: '100%',
     minHeight: 100,
     backgroundColor: '#fff'
   };
   return (
-    <div style={{ ...defaultStyle, ...style }} className={className}>
+    <div
+      style={{ ...defaultStyle, ...style }}
+      className={`${className} ${isDev ? devClassName : ''}`}
+    >
       {children}
     </div>
   );
