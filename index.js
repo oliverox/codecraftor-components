4a67ac984336912bac75eca80c3729eee3ec3ec7
Oliver Oxenham <oliver@cardcity.co>
2018-12-04 08:44:01 +0400

    - removed SpinnerOverlay

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import Heading from './Heading/Heading';
 import Span from './Span/Span';
 import Container from './Container/Container';
-import SpinnerOverlay from './SpinnerOverlay/SpinnerOverlay';
 
-const modules = { Heading, Span, Container, SpinnerOverlay };
+const modules = { Heading, Span, Container };
 
 const componentList = {
   Container: {
@@ -38,9 +37,3 @@ const componentList = {
 
 export default componentList;
 export { modules };
-
-// React -> Blueprint Components
-// import Heading from './react/blueprint/Heading/Heading';
-// import Button from './react/blueprint/Button/Button';
-
-// export { SpinnerOverlay, Heading, Button };
